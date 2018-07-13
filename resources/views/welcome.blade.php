55e76e750a7d1bbf1c1913829827949ef7d396aa
Purnama Yasa <yasapurnama@gmail.com>
2018-07-13 13:45:51 +0800

    edit home title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title> Implementation of Website Audit with Webscraping Technique </title>
+    <title> Implementation of Website Security Audit with Webscraping Technique </title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon" />
